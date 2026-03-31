689eb59a4128dbe54a32ea3a9c61a4e9387fbc32
Asafe Orneles <asafeorneles@gmail.com>
2026-03-30 23:24:31 -0300

    Initial commit

---
+++ src/main/java/com/asafeorneles/CacheFilmes/CacheFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.asafeorneles.CacheFilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asafeorneles/CacheFilmes/CacheFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asafeorneles.CacheFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
