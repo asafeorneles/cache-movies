7c79462177bd2b47a186f80b18cf8558760e4e5f
Asafe Orneles <asafeorneles@gmail.com>
2026-04-03 03:25:59 -0300

    feat: Criação do método para buscar os assentos pelo roomId.

--- src/main/java/com/asafeorneles/CacheFilmes/repositories/SeatRepository.java
+++ src/main/java/com/asafeorneles/CacheFilmes/repositories/SeatRepository.java
@@ -4,8 +4,14 @@ import com.asafeorneles.CacheFilmes.entities.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, UUID> {
+    Optional<Seat> findByRowNumberAndColumnNumberAndRoom_RoomId(int rowNumber, int columnNumber, UUID roomId);
+
+    List<Seat> findByRoom_RoomId(UUID roomId);
 }
+
