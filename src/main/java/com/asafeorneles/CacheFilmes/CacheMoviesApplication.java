4b6cbdf99ebd0f0700a1a64c0e411b0293d0886c
Asafe Orneles <asafeorneles@gmail.com>
2026-04-06 20:29:06 -0300

    refactor: Alteração no nome do Projeto. Alterei as pastas e arquivos necessários para atender ao novo nome do projeto, cache-movies.

--- src/main/java/com/asafeorneles/CacheFilmes/CacheMoviesApplication.java
+++ src/main/java/com/asafeorneles/CacheFilmes/CacheMoviesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CacheFilmesApplication {
+public class CacheMoviesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CacheFilmesApplication.class, args);
+		SpringApplication.run(CacheMoviesApplication.class, args);
 	}
 
 }
